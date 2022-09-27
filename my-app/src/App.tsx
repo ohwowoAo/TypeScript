dd57e24aa5111cb91bb856258b50ee1a853feb3a
ohwowoAo <sbjdnm@gmail.com>
2022-09-27 18:42:21 +0900

    Feat: Save the information need to login

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,48 +1,133 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
-const App = () => {
-  const [startTxt, setStartTxt ] = useState('GAME START~!');
+interface loginInput {
+  id : string,
+  pw : string
+}
 
-  const starttxt = ['가위' , '바위', '보'];
-  let i = 0;
-  const second = () => {
-    let setStart = setInterval(function() {
-      setStartTxt(starttxt[i])
-      i ++;
-      if( i === 3 ){
-        clearInterval(setStart);
-        
-      }
-    }, 1000);
+const App = () => {
+  const [userInput, setUserInput] = useState<loginInput>({id: '', pw:''})
+  const cursorId = useRef<HTMLInputElement>(null);
+  const cursorPw = useRef<HTMLInputElement>(null);
+
+  const changeInput = (e : React.ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = e.target;
+    setUserInput({...userInput, [name]: value} )
   }
+  // const inputpw = (e : {target : HTMLInputElement}) => {
+  //   setUserPw(e.target.value )
+  // }
+  const doLogin = () => {
+    if(userInput.id === ''){
+      alert('아이디를 입력해주세요');
+      if (cursorId.current) {
+        cursorId.current.focus()
+      }
+    }else if(userInput.pw === ''){
+      alert('비밀번호를 입력해주세요');
+      if (cursorPw.current) {
+        cursorPw.current.focus()
+      }
+    }else{
+      alert('로그인');
+    }
+  }
+  console.log(userInput)
   
+  useEffect(()=>{
+    if (cursorId.current) {
+      cursorId.current.focus()
+    }
+  },[])
+
   return (
     <Wrap>
-      <Start onClick={second}>{startTxt}</Start>
-      {/* <h2 ref={text}></h2> */}
-      <Hand>가위</Hand>
-      <Hand>바위</Hand>
-      <Hand>보</Hand>
-      {/* <p>게임 방법: 가위바위보 글자가 사라지면 위 버튼 중 내고 싶은 버튼을 누르세요.</p> */}
+      <WrapInner>
+        <Logo><img src= {process.env.PUBLIC_URL + '/images/logo.png'} alt='' style={{width: 200}}/></Logo>
+        <Form>
+          <Id>
+            <p>아이디</p>
+            <input ref={cursorId} name="id" placeholder="아이디 입력하세요" onChange={changeInput}></input>
+          </Id>
+          <PW>
+            <p>비밀번호</p>
+            <input ref={cursorPw} name="pw" placeholder="비밀번호를 입력하세요" onChange={changeInput}></input>
+          </PW>
+          
+          <LoginBtn onClick={doLogin}>
+            로그인
+          </LoginBtn>
+        </Form>
+      </WrapInner>
     </Wrap>
   );
 };
 
 const Wrap = styled.div`
-  padding: 80px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #e6e9ed;
+`;
+const WrapInner = styled.div`
+  position: relative;
+  width: 300px;
+  height: 500px;
+  margin: 0 auto;
+  padding: 0 30px;
+  background: #fafafa;
+  box-shadow: 0 13px 27px -5px rgb(50 50 93 / 25%), 0 8px 16px -8px rgb(0 0 0 / 30%), 0 -6px 16px -6px rgb(0 0 0 / 3%);
+  border-radius: 25px;
+`;
+const Logo = styled.div`
+  padding: 80px 0 40px;
   text-align: center;
 `;
-
-const Start = styled.button`
-  display: block;
-  margin: 0 auto 80px;
-  padding: 20px;
-  font-size: 30px;
+const Form = styled.div`
+  text-align: center;
 `;
-
-const Hand = styled.button`
-  margin: 0 10px;
+const Id = styled.div`
+  text-align: left;
+  > p {
+    font-size: 0.8rem;
+    margin: 20px 0 6px;
+  }
+  > input{
+    width: calc(100% - 26px);
+    height: 38px;
+    padding: 0 13px;
+    border-radius: 30px;
+    border: 1px solid #a9a9a9;
+    font-size: 0.7rem;
+  }
 `;
-
+const PW = styled.div`
+  text-align: left;
+  margin: 30px 0 0;
+  > p {
+    font-size: 0.8rem;
+    margin: 0 0 6px;
+  }
+  > input{
+    width: calc(100% - 26px);
+    height: 38px;
+    padding: 0 13px;
+    border-radius: 30px;
+    border: 1px solid #a9a9a9;
+    font-size: 0.7rem;
+  }
+`;
+const LoginBtn = styled.button`
+  position: absolute;
+  left: 30px;
+  bottom: 20px;
+  width: calc(100% - 60px);
+  height: 38px;
+  background-color: #0081c7;
+  border-radius: 30px;
+  border: 0;
+  color: #fff;
+`;
 export default App;
